b8ce04ce9854ff94c944ffadf29e8109249d085c
Shemaiah Hohn <shemaiah.hohn@gmail.com>
2026-03-11 10:50:48 -0400

    Secure Firebase API key using environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-9961019808-18897",
-  "appId": "1:307378373590:web:c232c4a13f32a03b1f9a77",
-  "apiKey": "AIzaSyAxoZeHq5brNNj508uYdW9ESSBEJHPq6Ck",
-  "authDomain": "studio-9961019808-18897.firebaseapp.com",
-  "storageBucket": "studio-9961019808-18897.firebasestorage.app",
-  "messagingSenderId": "307378373590",
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   "measurementId": ""
 };

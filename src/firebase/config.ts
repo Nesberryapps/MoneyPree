c7e03fcfe630b1ed13eae5abf3a6199a73798a8d
Shemaiah Hohn <shemaiah.hohn@gmail.com>
2026-03-11 10:45:39 -0400

    Update Firebase configuration with correct API key and missing fields

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,9 @@
 export const firebaseConfig = {
   "projectId": "studio-9961019808-18897",
   "appId": "1:307378373590:web:c232c4a13f32a03b1f9a77",
-  "apiKey": "AIzaSyBHDIleSPx-2p9fLRMMTwt37Kunay13fic",
+  "apiKey": "AIzaSyAxoZeHq5brNNj508uYdW9ESSBEJHPq6Ck",
   "authDomain": "studio-9961019808-18897.firebaseapp.com",
+  "storageBucket": "studio-9961019808-18897.firebasestorage.app",
+  "messagingSenderId": "307378373590",
   "measurementId": ""
 };
